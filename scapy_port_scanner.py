d3211344abfca7bdef1eb5b8a2c61763ca9155bb
Gill-Singh-A <amansg22@iitk.ac.in>
2024-06-30 07:24:07 +0530

    Corrected Reading of Port Scan Result

--- scapy_port_scanner.py
+++ scapy_port_scanner.py
@@ -84,7 +84,7 @@ if __name__ == "__main__":
         except:
             display('-', "Error in Reading the File")
             exit(0)
-        for target, open_ports in result.items():
+        for target, open_ports in result["result"].items():
             display(':', f"Target     = {target}")
             display(':', f"Open Ports = {len(open_ports)}")
             print(f"{Fore.GREEN}Open Ports{Fore.RESET}\n{'-'*10}{Fore.CYAN}")
