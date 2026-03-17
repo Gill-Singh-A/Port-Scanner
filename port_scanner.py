b00af2522fd1e6a2fdf1fdb22b1dc2c185311b45
Gill.Singh.A <amansg22@iitk.ac.in>
2026-03-17 21:03:31 +0530

    Shifted from Multiprocessing to Multithreading

--- port_scanner.py
+++ port_scanner.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
-import socket, os
+import socket
+from shlex import split
 from datetime import date
-from optparse import OptionParser
+from argparse import ArgumentParser
 from pickle import load, dump
-from multiprocessing import Pool, cpu_count, Lock
+from queue import Queue, Empty
+from threading import Thread, Lock
+from subprocess import run, DEVNULL
 from colorama import Fore, Back, Style
 from time import strftime, localtime, time
 
@@ -16,46 +19,61 @@ status_color = {
     ' ': Fore.WHITE,
 }
 
+ping_hosts = True
+lock = Lock()
+thread_count = 100
+
 def get_time():
     return strftime("%H:%M:%S", localtime())
 def display(status, data):
     print(f"{status_color[status]}[{status}] {Fore.BLUE}[{date.today()} {get_time()}] {status_color[status]}{Style.BRIGHT}{data}{Fore.RESET}{Style.RESET_ALL}")
 
-def get_arguments(*args):
-    parser = OptionParser()
-    for arg in args:
-        parser.add_option(arg[0], arg[1], dest=arg[2], help=arg[3])
-    return parser.parse_args()[0]
-
-ping_hosts = True
-lock = Lock()
+def get_arguments():
+    description = "Port Scanner"
+    parser = ArgumentParser(description=description)
+    parser.add_argument('-t', "--target", type=str, help="IP Address/Addresses of the Target/Targets to scan Ports (seperated by ',')")
+    parser.add_argument('-p', "--port", type=str, help="Port/Ports (seperated by ',') to scan")
+    parser.add_argument('-s', "--port-range", type=str, help="Range of Ports to scan (seperated by '-', start-stop)")
+    parser.add_argument('-P', "--ping", action="store_true", help=f"Ping to check Alive Hosts (True/False, Default={ping_hosts})")
+    parser.add_argument('-d', "--timeout", type=float, help="Timeout for Single Port Scan")
+    parser.add_argument('-T', "--threads", type=int, help=f"Number of Threads for Port Scanning (Default={thread_count})", default=thread_count)
+    parser.add_argument('-l', "--load", type=str, help="Load Targets from a file")
+    parser.add_argument('-r', "--read", type=str, help="File to read a Previous Scan Result")
+    parser.add_argument('-w', "--write", type=str, help="Dump the output to a File (Optional)", default=f"{date.today()} {strftime('%H_%M_%S', localtime())}")
+    return parser.parse_args()
 
 class PortScanner():
-    def __init__(self, hosts, ports = [], timeout=-1):
+    def __init__(self, hosts, ports=[], thread_count=100, timeout=None):
         self.hosts = hosts
         self.timeout = timeout
         if ports == []:
             self.ports = list(range(0, 65537))
         else:
             self.ports = ports
-        self.host_down = []
+        self.thread_count = thread_count
+        self.up_hosts = []
+        self.down_hosts = []
         self.open_ports = {host:[] for host in self.hosts}
-    def checkHost(self, host):
-        return os.system(f"ping -c 1 {host} >/dev/null") == 0
-    def checkHosts(self, hosts):
-        up_hosts, down_hosts = [], []
-        for host in hosts:
+    def checkHost(self, host, count=4, timeout=2):
+        return run(split(f"ping -w {timeout} -c {count} {host}"), shell=False, stdout=DEVNULL, stderr=DEVNULL).returncode == 0
+    def checkHosts(self, queue):
+        while True:
+            try:
+                host = queue.get_nowait()
+            except Empty:
+                break
             if self.checkHost(host):
-                up_hosts.append(host)
+                with lock:
+                    display('+', f"Host {Back.MAGENTA}{host}{Back.RESET} Alive")
+                    self.up_hosts.append(host)
             else:
                 with lock:
                     display('*', f"Host {Back.MAGENTA}{host}{Back.RESET} Unreachable")
-                down_hosts.append(host)
-        return up_hosts, down_hosts
+                    self.down_hosts.append(host)
     def checkPort(self, host, port):
         try:
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            if self.timeout != -1:
+            if self.timeout != None:
                 socket.setdefaulttimeout(self.timeout)
             result = self.socket.connect_ex((host, port))
         except:
@@ -64,60 +82,52 @@ class PortScanner():
             if result == 0:
                 return True
             self.socket.close()
-    def scanner(self, hosts, ports):
-        open_ports = {host:[] for host in hosts}
-        for host in hosts:
+    def scanner(self, queue, ports):
+        while True:
+            try:
+                host = queue.get_nowait()
+            except Empty:
+                break
             for port in ports:
                 status = self.checkPort(host, port)
                 if status:
                     with lock:
+                        self.open_ports[host].append(port)
                         display(':', f"Open => {Back.MAGENTA}{host}:{port}{Back.RESET}")
-                    open_ports[host].append(port)
-        return open_ports
     def scan(self):
         t1 = time()
-        thread_count = cpu_count()
-        display(':', f"Detecting Alive Hosts with {Back.MAGENTA}{thread_count} Threads{Back.RESET}")
         if ping_hosts:
-            pool = Pool(thread_count)
-            host_count = len(self.hosts)
-            host_divisions = [self.hosts[group*host_count//thread_count: (group+1)*host_count//thread_count] for group in range(thread_count)]
+            display(':', f"Detecting Alive Hosts with {Back.MAGENTA}{thread_count} Threads{Back.RESET}")
             threads = []
-            for host_division in host_divisions:
-                threads.append(pool.apply_async(self.checkHosts, (host_division, )))
+            queue = Queue()
+            for host in self.hosts:
+                queue.put(host)
+            for thread_index in range(self.thread_count):
+                threads.append(Thread(target=self.checkHosts, args=(queue, )))
+                threads[-1].start()
             for thread in threads:
-                up_hosts, down_hosts = thread.get()
-                for host in down_hosts:
-                    self.open_ports.pop(host)
-                    self.hosts.remove(host)
-            pool.close()
-            pool.join()
+                thread.join()
+            for host in self.down_hosts:
+                self.open_ports.pop(host)
+                self.hosts.remove(host)
             display('+', f"Total Alive Hosts = {Back.MAGENTA}{len(self.open_ports)}{Back.RESET}")
         display(':', f"Starting Port Scanning {Back.MAGENTA}{thread_count} Threads{Back.RESET}")
-        pool = Pool(thread_count)
-        host_count = len(self.hosts)
-        host_divisions = [self.hosts[group*host_count//thread_count: (group+1)*host_count//thread_count] for group in range(thread_count)]
         threads = []
-        for host_division in host_divisions:
-            threads.append(pool.apply_async(self.scanner, (host_division, self.ports)))
+        queue = Queue()
+        for host in self.hosts:
+            queue.put(host)
+        for thread_index in range(self.thread_count):
+            threads.append(Thread(target=self.scanner, args=(queue, self.ports, )))
+            threads[-1].start()
         for thread in threads:
-            current_thread_open_ports = thread.get()
-            for host, ports in current_thread_open_ports.items():
-                self.open_ports[host].extend(ports)
-        pool.close()
-        pool.join()
+            thread.join()
+        for thread in threads:
+            thread.get()
         t2 = time()
         return self.open_ports, self.host_down, t2-t1
 
 if __name__ == "__main__":
-    data = get_arguments(('-t', "--target", "target", "IP Address/Addresses of the Target/Targets to scan Ports (seperated by ',')"),
-                         ('-p', "--port", "port", "Port/Ports (seperated by ',') to scan"),
-                         ('-s', "--port-range", "port_range", "Range of Ports to scan (seperated by '-', start-stop)"),
-                         ('-P', "--ping", "ping", f"Ping to check Alive Hosts (True/False, Default={ping_hosts})"),
-                         ('-d', "--timeout", "timeout", "Timeout for Single Port Scan"),
-                         ('-l', "--load", "load", "Load Targets from a file"),
-                         ('-r', '--read', "read", "File to read a Previous Scan Result"),
-                         ('-w', "--write", "write", "Dump the output to a File (Optional)"))
+    data = get_arguments()
     if data.read:
         try:
             with open(data.read, 'rb') as file:
@@ -166,17 +176,9 @@ if __name__ == "__main__":
     else:
         ports = data.port.split(',')
         ports = [int(port.strip()) for port in ports]
-    if data.ping == "False":
-        ping_hosts = False
-    if not data.timeout:
-        data.timeout = -1
-    else:
-        data.timeout = int(data.timeout)
-    if not data.write:
-        data.write = f"{date.today()} {strftime('%H_%M_%S', localtime())}"
     result = {}
     scanner = PortScanner(data.target, ports=ports, timeout=data.timeout)
-    display(':', f"Starting Port Scan on {Back.MAGENTA}{len(data.target)} Targets{Back.RESET} for {Back.MAGENTA}{len(ports)}{Back.RESET} ports with {Back.MAGENTA}{cpu_count()} Threads{Back.RESET} ")
+    display(':', f"Starting Port Scan on {Back.MAGENTA}{len(data.target)} Targets{Back.RESET} for {Back.MAGENTA}{len(ports)}{Back.RESET} ports with {Back.MAGENTA}{data.threads} Threads{Back.RESET} ")
     result, host_down, time_taken = scanner.scan()
     display('+', f"Port Scan Finished!\n")
     display(':', f"Time Taken for Full Scan = {Back.MAGENTA}{time_taken}{Back.RESET}\n")
